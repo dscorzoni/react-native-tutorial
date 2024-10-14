c1aa55b7bd9fec54235d30ffdc9247ed93e59cba
dscorzoni <danilo.scorzoni@gmail.com>
2024-10-14 14:05:27 -0300

    feat: added status bar object

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
 import { Stack } from 'expo-router';
+import { setStatusBarStyle, StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle('light');
+    });
+  }, []);
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
 
